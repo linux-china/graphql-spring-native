ada7991a38836a2578da10f12cccc8afce0958ac
linux_china <libing.chen@gmail.com>
2022-05-30 14:48:18 -0700

    chore: add graphql related class for reflect

--- src/main/java/org/springframework/experimental/graphql/GraphQlNativeHints.java
+++ src/main/java/org/springframework/experimental/graphql/GraphQlNativeHints.java
@@ -1,6 +1,16 @@
 package org.springframework.experimental.graphql;
 
-import org.springframework.graphql.data.method.annotation.QueryMapping;
+import graphql.GraphQL;
+import graphql.analysis.QueryVisitorFieldArgumentEnvironment;
+import graphql.analysis.QueryVisitorFieldArgumentInputValue;
+import graphql.execution.Execution;
+import graphql.execution.nextgen.result.RootExecutionResultNode;
+import graphql.language.*;
+import graphql.parser.ParserOptions;
+import graphql.schema.*;
+import graphql.schema.validation.SchemaValidationErrorCollector;
+import graphql.util.NodeAdapter;
+import graphql.util.NodeZipper;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.nativex.hint.JdkProxyHint;
 import org.springframework.nativex.hint.ResourceHint;
@@ -13,9 +23,84 @@ import org.springframework.nativex.type.NativeConfiguration;
  * @author <a href="mailto:josh@joshlong.com">Josh Long</a>
  */
 @TypeHint(
+        typeNames = {
+                "graphql.analysis.QueryTraversalContext",
+                "graphql.schema.idl.SchemaParseOrder",
+        },
         types = {
-                QueryMapping.class,
-                SchemaMapping.class
+                Argument.class,
+                ArrayValue.class,
+                BooleanValue.class,
+                DataFetchingEnvironment.class,
+                Directive.class,
+                DirectiveDefinition.class,
+                DirectiveLocation.class,
+                Document.class,
+                EnumTypeDefinition.class,
+                EnumTypeExtensionDefinition.class,
+                EnumValue.class,
+                EnumValueDefinition.class,
+                Execution.class,
+                Field.class,
+                FieldDefinition.class,
+                FloatValue.class,
+                FragmentDefinition.class,
+                FragmentSpread.class,
+                GraphQL.class,
+                GraphQLArgument.class,
+                GraphQLCodeRegistry.Builder.class,
+                GraphQLDirective.class,
+                GraphQLEnumType.class,
+                GraphQLEnumValueDefinition.class,
+                GraphQLFieldDefinition.class,
+                GraphQLInputObjectField.class,
+                GraphQLInputObjectType.class,
+                GraphQLInterfaceType.class,
+                GraphQLList.class,
+                GraphQLNamedType.class,
+                GraphQLNonNull.class,
+                GraphQLObjectType.class,
+                GraphQLOutputType.class,
+                GraphQLScalarType.class,
+                GraphQLSchema.class,
+                GraphQLSchemaElement.class,
+                GraphQLUnionType.class,
+                ImplementingTypeDefinition.class,
+                InlineFragment.class,
+                InputObjectTypeDefinition.class,
+                InputObjectTypeExtensionDefinition.class,
+                InputValueDefinition.class,
+                IntValue.class,
+                InterfaceTypeDefinition.class,
+                InterfaceTypeExtensionDefinition.class,
+                ListType.class,
+                NodeAdapter.class,
+                NodeZipper.class,
+                NonNullType.class,
+                NullValue.class,
+                ObjectField.class,
+                ObjectTypeDefinition.class,
+                ObjectTypeExtensionDefinition.class,
+                ObjectValue.class,
+                OperationDefinition.class,
+                OperationTypeDefinition.class,
+                ParserOptions.class,
+                QueryVisitorFieldArgumentEnvironment.class,
+                QueryVisitorFieldArgumentInputValue.class,
+                RootExecutionResultNode.class,
+                ScalarTypeDefinition.class,
+                ScalarTypeExtensionDefinition.class,
+                SchemaDefinition.class,
+                SchemaExtensionDefinition.class,
+                SchemaValidationErrorCollector.class,
+                SelectionSet.class,
+                StringValue.class,
+                TypeDefinition.class,
+                TypeName.class,
+                UnionTypeDefinition.class,
+                UnionTypeExtensionDefinition.class,
+                VariableDefinition.class,
+                VariableReference.class,
         }
 )
 @ResourceHint(patterns = {"graphiql/index.html"})
